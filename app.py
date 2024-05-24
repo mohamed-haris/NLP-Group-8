e6df653b6ae5164da9d5b77b425dee0b68e95c4c
Mohamed Haris Khajamainudeen <mohamedhariskhajamainudeen@Mohamed-Haris-MacBook-Pro.local>
2024-05-24 11:55:32 +0100

    trigger build 3

--- app.py
+++ app.py
@@ -10,13 +10,28 @@ import zipfile
 MODEL_OF_CHOICE = "BERT"
 file_id = '1Fa1CdPZTrhwjSPEZJTBQN_BI4SW3uHG9'
 model_url = f'https://drive.google.com/uc?id={file_id}'
-local_zip_path = 'bert_model.pth.zip'
+local_zip_path = 'bert_model.zip'
 local_model_dir = 'bert_model.pth'
 
-if not os.path.exists(local_model_dir):
-    gdown.download(model_url, local_zip_path, quiet=False)
-    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
-        zip_ref.extractall()
+def download_and_extract_model():
+    if not os.path.exists(local_model_dir):
+        gdown.download(model_url, local_zip_path, quiet=False)
+        with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+            zip_ref.extractall()
+        os.remove(local_zip_path)
+
+app = Flask(__name__)
+
+@app.before_first_request
+def setup():
+    download_and_extract_model()
+
+logger = logging.getLogger('BERT_NER_Predictions')
+logger.setLevel(logging.ERROR)
+handler = logging.FileHandler('app.log')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModelForTokenClassification.from_pretrained(local_model_dir)
@@ -27,23 +42,17 @@ classifier_pipeline = pipeline(
     aggregation_strategy='max'
 )
 
-app = Flask(__name__)
-
-# Setup structured logging
-logger = logging.getLogger('BERT_NER_Predictions')
-logger.setLevel(logging.WARNING)
-handler = logging.FileHandler('app.log')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+@app.route('/')
+def index():
+    return "Hello from Group 8 NLP team!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     request_data = request.get_json()
     text = request_data.get('text', '')
-    
-    logger.warning(json.dumps({'event': 'received_text', 'text': text}))
-    
+
+    logger.error(json.dumps({'event': 'received_text', 'text': text}))
+
     with torch.no_grad():
         outputs = classifier_pipeline(text)
         output = {}
@@ -52,10 +61,10 @@ def predict():
             del item['start']
             del item['end']
             output[f"word-{i}"] = item
-        
+
         json_out = json.dumps(output)
-        logger.warning(json.dumps({'event': 'outputs', 'json_out': json_out}))
-        
+        logger.error(json.dumps({'event': 'outputs', 'json_out': json_out}))
+
     return jsonify(output)
 
 if __name__ == '__main__':
